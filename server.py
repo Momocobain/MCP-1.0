c1ebc7469f6b36fe3be3943d834cab9f053dfc39
Momocobain <themomo0798@gmail.com>
2024-08-23 04:30:31 +0000

    Mundo guardado 2024-08-23

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # Versiones disponibles
-# forge, 1mohist, fabric, vanilla, paper
+# forge, 1112mohist, fabric, vanilla, paper
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
